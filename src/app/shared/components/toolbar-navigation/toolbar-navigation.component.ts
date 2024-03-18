7c2658ab632072f1f686399e324e4374f35d1b33
Rafael Ulisses <ruds.devel@gmail.com>
2024-03-18 11:14:19 -0300

    Logout

--- src/app/shared/components/toolbar-navigation/toolbar-navigation.component.ts
+++ src/app/shared/components/toolbar-navigation/toolbar-navigation.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-toolbar-navigation',
@@ -6,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './toolbar-navigation.component.scss'
 })
 export class ToolbarNavigationComponent {
+
+  constructor(
+    private cookieService: CookieService,
+    private router: Router,
+  ) {}
+
+  handleLogout():void {
+    this.cookieService.delete(environment.USER_COOKIE)
+    void this.router.navigate(['/home'])
+  }
 
 }
