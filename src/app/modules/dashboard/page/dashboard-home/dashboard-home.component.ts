d1a34e1a5799e19b0ad01c49843115e16c9a67ed
Rafael Ulisses <ruds.devel@gmail.com>
2024-03-19 15:24:33 -0300

    Gráfico de quantidade produtos em estoque

--- src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
+++ src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
@@ -6,8 +6,10 @@ import { MessageService } from 'primeng/api';
 import { ProductsDataTransferService } from '../../../../shared/services/products/products-data-transfer.service';
 import { ProductsService } from '../../../../services/products/products.service';
 
-import { GetAllProductsResponse } from '../../../../models/interfaces/products/response/GetAllProductsResponse';
 import { Observable, Subject, takeUntil } from 'rxjs';
+import { ChartData, ChartOptions } from 'chart.js';
+
+import { GetAllProductsResponse } from '../../../../models/interfaces/products/response/GetAllProductsResponse';
 
 @Component({
   selector: 'app-dashboard-home',
@@ -19,6 +21,9 @@ export class DashboardHomeComponent implements OnInit, OnDestroy{
   private readonly destroy$: Subject<void> = new Subject()
   public productList: Array<GetAllProductsResponse> = []
   public observbleTest$ = new Observable()
+
+  public productChartData!: ChartData
+  public productChartOptions!: ChartOptions
 
   constructor(
     private productService: ProductsService,
@@ -47,6 +52,61 @@ export class DashboardHomeComponent implements OnInit, OnDestroy{
     if(response.length) {
       this.productList = response
       this.productsDataTransferService.setProductsDatas(this.productList);
+      this.setProductsChartConfig()
+    }
+  }
+
+  private setProductsChartConfig(): void {
+    if(!this.productList?.length) return
+    const documentStyle = getComputedStyle(document.documentElement)
+    const textColor =  documentStyle.getPropertyValue('--text-color')
+    const textColorSecondary =  documentStyle.getPropertyValue('--text-color-secondary')
+    const surfaceBorder =  documentStyle.getPropertyValue('--surface-border')
+    this.productChartData = {
+      labels: this.productList.map(p => p?.name),
+      datasets:[{
+        label: 'quantidades',
+        backgroundColor: documentStyle.getPropertyValue('--indigo-400'),
+        borderColor: documentStyle.getPropertyValue('--indigo-400'),
+        hoverBackgroundColor: documentStyle.getPropertyValue('--indigo-500'),
+        data: this.productList.map(p => p?.amount)
+      }]
+    }
+
+    this.productChartOptions = {
+      maintainAspectRatio: false,
+      aspectRatio: 0.8,
+      plugins: {
+        legend: {
+          labels: {
+            color: textColor
+          }
+        }
+      },
+      scales: {
+        x: {
+          ticks: {
+            color: textColorSecondary,
+            font: {
+              weight: 500,
+            },
+          },
+          grid: {
+            color: surfaceBorder
+          }
+        },
+        y: {
+          ticks: {
+            color: textColorSecondary,
+            /* font: {
+              weight: 500,
+            }, */
+          },
+          grid: {
+            color: surfaceBorder
+          }
+        }
+      }
     }
   }
 
